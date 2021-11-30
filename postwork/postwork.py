3462770b675c363ded84863b64fc7e36d2527b1b
netoxdw <neto.guevara.tapia@gmail.com>
2021-11-30 14:49:43 -0600

    iniciando proyecto

---
+++ postwork/postwork.py
@@ -0,0 +1,32 @@
+# Calculadora de intereses de una tarjeta de credito
+
+from sesion2.postwork import crea_tarjeta
+
+
+print('Ingrese los siguientes datos.')
+
+print('Nombre de la tarjeta:')
+tarjeta = input ()
+
+
+print('Tasa de interes en %:')
+ti = float(input ())/100
+im = ti/12
+
+print('Deuda:')
+deudaparcial = float(input ())
+deudatotal = deudaparcial * (1 + im)
+
+print(f'Deuda total:{deudatotal}')
+
+print('Monto de pago:')
+pago = float(input())
+
+if  pago <= deudatotal:
+    print('Gracias por tu pago')
+else:
+    print('El pago es mayor a lo adeudado')
+
+diferencia = deudatotal - pago
+nueva_deuda = diferencia * (1+im)
+print(f'Nueva deuda: ${nueva_deuda}')
